1afa2c790482ab2a46afc674b52baf31bd034e7a
Nick Dunlap <nwdunlap17@gmail.com>
2019-08-05 14:31:42 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/name" do
+        status 200
+        "My name is Nick"
+    end
+    get "/hometown" do
+        status 200
+        "My hometown is Maple Valley"
+    end
+    get "/favorite-song" do
+        status 200
+        "My favorite song is No Rest For The Wicked"
+    end
+
 end
